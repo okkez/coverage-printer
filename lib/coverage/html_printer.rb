8b49f577cd9507993dfe61c82aa8641cba397020
okkez <okkez000@gmail.com>
2011-03-18 00:35:39 +0900

    add Coverage::HTMLPrinter class, but not work yet.

---
+++ lib/coverage/html_printer.rb
@@ -0,0 +1,19 @@
+require 'coverage'
+
+module Coverage
+  class HTMLPrinter
+    def initialize
+    end
+
+    def print(result)
+      result.each do |path, counts|
+        next if /test-unit/ =~ path
+        source = Pathname(path)
+        source.each_line.with_index.zip(counts) do |(line, index), count|
+          
+        end
+      end
+    end
+  end
+end
+
