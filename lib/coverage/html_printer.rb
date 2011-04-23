bbd7a5cc87c38fe2ad955ef1c0bcd75d639473f2
okkez <okkez000@gmail.com>
2011-04-23 22:34:00 +0900

    add Coverage::HTMLPrinter::{PathSettings,Utility}.

--- lib/coverage/html_printer.rb
+++ lib/coverage/html_printer.rb
@@ -7,37 +7,89 @@ require 'forwardable'
 module Coverage
   class HTMLPrinter
 
+    module Utility
+      def stylesheet(name, html_filename)
+        css = output_directory + "#{name}.css"
+        path = css.relative_path_from(Pathname(html_filename).dirname)
+        %Q!<link rel="stylesheet" type="text/css" href="#{path.to_s}"/>!
+      end
+
+      def javascript(name, html_filename)
+        js = output_directory + "#{name}.js"
+        path = js.relative_path_from(Pathname(html_filename).dirname)
+        %Q!<script src="#{path.to_s}"></script>!
+      end
+
+      def coverage_bar(coverage)
+        %Q!<div class="bar-container"><div style="width: #{coverage}%"></div></div>#{coverage}%!
+      end
+    end
+
+    class PathSettings
+
+      attr_reader :output_directory
+
+      def initialize(output_directory)
+        @output_directory = output_directory
+      end
+
+      def base_directory
+        @base_directory ||= Pathname.pwd
+      end
+
+      def lib_base_directory
+        @lib_base_directory ||= Pathname(__FILE__).dirname.parent.parent
+      end
+
+      def templates_directory
+        lib_base_directory + "data/templates"
+      end
+
+      def javascripts_directory
+        lib_base_directory + "data/javascripts"
+      end
+
+      def stylesheets_directory
+        lib_base_directory + "data/stylesheets"
+      end
+    end
+
+    extend Forwardable
+
     attr_accessor :output_directory, :project_name
-    attr_reader :lib_base_directory, :base_directory
+    def_delegators(:@path_settings, :base_directory, :lib_base_directory, :output_directory,
+                   :templates_directory, :javascripts_directory, :stylesheets_directory)
 
     def initialize
       yield self if block_given?
-      @lib_base_directory = Pathname(__FILE__).dirname.parent.parent
-      @base_directory = Pathname.pwd
-      @output_directory ||=  @base_directory + "coverage"
-      FileUtils.mkdir_p(@output_directory)
-      @project_name ||= @base_directory.basename
+      output_directory ||=  Pathname.pwd + "coverage"
+      @path_settings = PathSettings.new(output_directory)
+      FileUtils.mkdir_p(output_directory)
+      @project_name ||= base_directory.basename
     end
 
     def print(result)
-      target_files = Dir.glob("#{@base_directory}/**/*.rb")
+      target_files = Dir.glob("#{base_directory}/**/*.rb")
       statistics_list = []
       files = []
       result.each do |path, counts|
         next unless target_files.include?(path)
-        next if Regexp.new("#{@base_directory}/(?:test|spec)") =~ path
-        files << Detail.new(@base_directory, path, counts)
+        next if Regexp.new("#{base_directory}/(?:test|spec)") =~ path
+        files << Detail.new(@path_settings, path, counts)
       end
-      files.each do |file|
-        print_detail(file)
-      end
+      # files.each do |file|
+      #   print_detail(file)
+      # end
+      files.each(&:print)
       print_index(files)
+      # index = Index.new(files)
+      # index.print
       install_files
     end
 
     def print_detail(file)
       erb = ERB.new(File.read(templates_directory + "detail.html.erb"), nil, '-')
-      html_filepath = @output_directory + file.html_filename
+      html_filepath = output_directory + file.html_filename
       erb.filename = html_filepath.to_s
       FileUtils.mkdir_p(html_filepath.dirname)
       File.open(html_filepath, "wb+") do |html|
@@ -48,7 +100,7 @@ module Coverage
     def print_index(files)
       erb = ERB.new(File.read(templates_directory + "index.html.erb"), nil, '-')
       erb.filename = "index.html"
-      index_path = @output_directory + "index.html"
+      index_path = output_directory + "index.html"
       File.open(index_path, "wb+") do |html|
         html.puts(erb.result(binding))
       end
@@ -63,38 +115,20 @@ module Coverage
       end
     end
 
-    def templates_directory
-       lib_base_directory + "data/templates"
-    end
-
-    def javascripts_directory
-      lib_base_directory + "data/javascripts"
-    end
-
-    def stylesheets_directory
-      lib_base_directory + "data/stylesheets"
-    end
-
-    def stylesheet(name, html_filename)
-      css = @output_directory + "#{name}.css"
-      path = css.relative_path_from(Pathname(html_filename).dirname)
-      %Q!<link rel="stylesheet" type="text/css" href="#{path.to_s}"/>!
-    end
-
-    def javascript(name, html_filename)
-      js = @output_directory + "#{name}.js"
-      path = js.relative_path_from(Pathname(html_filename).dirname)
-      %Q!<script src="#{path.to_s}"></script>!
+    class Index
+      def print
+      end
     end
 
     class Detail
       extend Forwardable
+      include Utility
 
       def_delegators(:@statistics, :total, :lines_of_code)
       attr_reader :page_title
 
-      def initialize(base_directory, path, counts)
-        @base_directory = base_directory
+      def initialize(path_settings, path, counts)
+        @path_settings = path_settings
         @path = path
         @counts = counts
         @statistics = Coverage::Statistics.new(path, counts)
@@ -103,6 +137,16 @@ module Coverage
         @sources = []
         @source.each_line.with_index.zip(@counts) do |(line, index), count|
           @sources << Line.new(index + 1, line, count)
+        end
+      end
+
+      def print
+        erb = ERB.new(template_path, nil, '-')
+        path = @path_settings.output_directory + html_filename
+        erb.filename = path
+        FileUtils.mkdir_p(path.dirname)
+        File.open(path, "wb+") do |file|
+          file.puts(erb.result(binding))
         end
       end
 
@@ -131,9 +175,12 @@ module Coverage
         coverage_bar(@statistics.code_coverage)
       end
 
+      def print
+      end
+
       private
-      def coverage_bar(coverage)
-        %Q!<div class="bar-container"><div style="width: #{coverage}%"></div></div>#{coverage}%!
+      def template_path
+        @path_settings.templates_directory + "detail.html.erb"
       end
     end
 
